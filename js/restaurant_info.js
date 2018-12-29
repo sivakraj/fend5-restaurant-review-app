61ca52ad3fb2a7be63b399e5919bcdbb422caf43
Sivakumar Rajendran <emails2shiva@gmail.com>
2018-12-29 09:31:03 +0530

    Added accessibility and view ports to HTML files
    feat: Added accessibility(aria attributes) and view port meta tags
    
    Accessibility, especially aria attributes and tab indexes for smoother keyboard navigation has been added to index.html and restaurant.html files
    
    Also, Mapbox key has been added to restaurant_info.js file.

--- js/restaurant_info.js
+++ js/restaurant_info.js
@@ -22,7 +22,7 @@ initMap = () => {
         scrollWheelZoom: false
       });
       L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.jpg70?access_token={mapboxToken}', {
-        mapboxToken: '<your MAPBOX API KEY HERE>',
+        mapboxToken: 'pk.eyJ1Ijoic2l2YWtyYWoiLCJhIjoiY2pxOHUycm8xMGFxazQ5cGpiN2F2dDRveCJ9.kaf2GgUF4Uj2Wn76dwRDsQ',
         maxZoom: 18,
         attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
           '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
